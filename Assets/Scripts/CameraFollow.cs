8bd20fcd36a9e3537b0980abd329b8807e6e488c
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-11-04 14:53:05 +0000

    Added Camera Follow Script

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        transform.position = target.position + offset;
+        
+        
+
+    }
+}
