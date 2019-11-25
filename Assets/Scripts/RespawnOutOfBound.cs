992340db4a123bd52b6f14f4eccc58ef163802db
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-11-25 19:41:35 +0000

    Added respawn for falling out of map

---
+++ Assets/Scripts/RespawnOutOfBound.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnOutOfBound : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Vector3 respawnPos = new Vector3(0, 0.63f, 0);
+            other.transform.position = respawnPos;
+        }
+    }
+}
