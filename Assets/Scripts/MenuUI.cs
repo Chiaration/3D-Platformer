10c1eede76b1eebd11b6e67bd6f4a80158854157
Chiaration <45766347+Chiaration@users.noreply.github.com>
2019-11-28 16:21:13 +0000

    Menu UI bug fixed

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -5,6 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MenuUI : MonoBehaviour
 {
+    [SerializeField] private GameObject MenuObject;
+    [SerializeField] private GameObject LevelObject;
+    
+    bool MenuBool = true;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -24,7 +29,17 @@ public class MenuUI : MonoBehaviour
 
     public void SelectLevel()
     {
-        
+        if (MenuBool)
+        {
+            MenuObject.SetActive(false);
+            LevelObject.SetActive(true);
+            MenuBool = false;
+        } else if (!MenuBool)
+        {
+            MenuObject.SetActive(true);
+            LevelObject.SetActive(false);
+            MenuBool = true;
+        }
     }
 
     public void QuitGame()
